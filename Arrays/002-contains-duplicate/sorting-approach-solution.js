46902de9e8541b72c4d96f094fb2d4d54b86283f
Hammad Afzal <hammad.afzal.code@gmail.com>
2026-09-19 19:53:25 +0500

    sold cotains duplicate using sorting approach

---
+++ Arrays/002-contains-duplicate/sorting-approach-solution.js
@@ -0,0 +1,13 @@
+// two pointer solution of contains duplicate problem
+
+function constainsDuplicate(nums) {
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(constainsDuplicate([1, 2, 3, 1]));

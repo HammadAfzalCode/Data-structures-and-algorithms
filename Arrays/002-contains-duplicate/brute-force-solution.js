6babd3c4fa378139ac5637bf0e512a3aea557bc1
Hammad Afzal <hammad.afzal.code@gmail.com>
2026-09-19 19:20:42 +0500

    solved contains duplicate problem via brute force approach

---
+++ Arrays/002-contains-duplicate/brute-force-solution.js
@@ -0,0 +1,15 @@
+// brute force solution of contains duplicate
+
+function constainsDuplicate(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(constainsDuplicate([1, 2, 3, 1]));
+// time complexity is n^2
